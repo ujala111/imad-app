d381d4c2b66ec6703cf304200caa837fd62b22af
ujala111 <30621131+ujala111@users.noreply.github.com>
2017-08-14 16:39:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element= document.getElementById('main-text');
 element.innerHTML= 'New Value';
 
 //move the image.
-var image= document.getElementById('modi');
+var img= document.getElementById('modi');
 img.onclick= function() {
     img.style.marginLeft= '100px';
 } ;
