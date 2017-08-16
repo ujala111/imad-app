fc6540379d3d34516bb5ba99596b096bae614c8f
ujala111 <30621131+ujala111@users.noreply.github.com>
2017-08-16 12:36:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //counter code
 var button = document.getElementById("counter");
 var counter= 0;
-button.onClick= function(){
+button.onClick= function() {
     
     //make a request to the counter end point
     
